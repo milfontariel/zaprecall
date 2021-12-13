51b040bb3ee27d8f019627c5d9b7b7a7317bb028
Ariel <ariel1242009@gmail.com>
2021-12-13 14:45:59 -0300

    projeto finalizado

--- src/Initial/index.js
+++ src/Initial/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import logo from '../assets/logo.svg';
 import Header from '../Header';
 import Flashcards from '../Flashcards';
+import next from '../assets/next.svg';
 
 export default function Content({display}){
   let mostrar = [<Header />, <Flashcards display={display} />];
@@ -9,7 +10,7 @@ export default function Content({display}){
     <div className="content">
       <img src={logo} />
       <h1>ZapRecall</h1>
-      <button className='btn-initial' onClick={() => display(mostrar)}>Praticar React</button>
+      <button data-identifier="start-zap-recall" className='btn-initial' onClick={() => display(mostrar)}>Praticar React<img src={next} alt="next" /></button>
     </div>
   )
 }
